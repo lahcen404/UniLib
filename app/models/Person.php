75a45ebff810c4c3fb04f65adaafe01a7c6783d8
lahcen404 <lahcen.maskour2003@gmail.com>
2025-12-24 12:06:52 +0100

    create  Abstract Class Person

---
+++ app/models/Person.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once __DIR__ . '/../app/Enums/role.php';
+
+abstract class Person{
+
+    protected $id ;
+    protected $firstName;
+    protected $lastName;
+    protected $email;
+    protected $password;
+    protected Role $role;
+
+    public function __construct($id=null , $firstName=null , $lastName=null , $email=null , $password=null ){
+        $this->id = $id;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->email = $email;
+
+        if($password){
+            $this->setPassword($password);
+        }
+        
+        $this->role = Role::USER;
+    }
+
+    
+//getteers
+    public function getId(){ return $this-> id; }
+    public function getFirstName(){ return $this->firstName;}
+    public function getLastName(){ return $this->lastName;}
+    public function getEmail(){ return $this->email;}
+    public function getRole(){ return $this->role;}
+
+
+    //setters
+    public function setFirstName($fName){ $this->firstName = $fName;}
+    public function setLastName($lName){ $this->lastName = $lName;}
+    public function setEmail($email){ $this->email = $email;}
+    public function setPassword($password){$this->password = password_hash($password , PASSWORD_BCRYPT);}
+    public function setRole(Role $role){ $this->role = $role;}
+
+    // getfullname
+    public function getFullName(){return $this->firstName . ' ' . $this->lastName;}
+
+
+}
+
+
+?>
